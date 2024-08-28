43762e4c94f62126332b25fc62e073daab8347ad
John-Kyle-404 <jkylecadap@gmail.com>
2024-08-28 18:19:52 +1000

    test

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from '../login/login.component';
+import {AccountsComponent} from '../accounts/accounts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component:LoginComponent},
+    {path: 'accounts', component:AccountsComponent},
+
+];
