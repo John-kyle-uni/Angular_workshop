5cbd721232c29f42826335ba5c95929bb7f24262
John-Kyle-404 <jkylecadap@gmail.com>
2024-08-28 18:41:20 +1000

    test

--- my-app/src/login/login.component.ts
+++ my-app/src/login/login.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  // Hardcoded users
+  users = [
+    { username: 'admin', password: '12345' },
+    { username: 'user', password: 'user1' },
+    { username: 'group', password: 'group1' }
+  ];
+
+  constructor(private router: Router) {}
+
+  login() {
+    const user = this.users.find(u => u.username === this.username && u.password === this.password);
+
+    if (user) {
+      this.router.navigate(['/accounts']);
+    } else {
+      this.errorMessage = 'Invalid username or password';
+    }
+  }
 }
